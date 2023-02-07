449f4d85c3fc96c2530cc5c169506b6ba46a9c62
WGtechy <wills.grande@gmail.com>
2023-02-07 02:33:14 -0800

    Minor changes in the introPage

--- src/views/IntroPage.js
+++ src/views/IntroPage.js
@@ -156,10 +156,7 @@ const IntroPage = ({observer}) => {
             </div>
           </div>
           <div className='contentText'>
-          With Synando, you will see what's closest to you.
-           You will see who's closer to you.
-           You will have control and comfort to share personal stories,
-          and inspire your friends without feeling like you're talking to a stranger; control your world.
+          With Synando, you will see what's closest to you. You will see who's closer to you.  You will have control and comfort to share personal stories, and inspire your friends without feeling like you're talking to a stranger; control your world.
           </div>
           <div className="downloadIcons">
             {downloadIcons.map((item, i) => (
